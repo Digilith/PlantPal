03739946467ffd4e90e88090a5033caec29c4c9e
Digilith <digilith_@hotmail.com>
2022-06-09 03:20:44 +0300

    Swaped listview to recyclerview

--- app/src/main/java/com/digilith/plantpal/MainActivity.java
+++ app/src/main/java/com/digilith/plantpal/MainActivity.java
@@ -3,9 +3,17 @@ package com.digilith.plantpal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    private MaterialButton addPlantBtn;
+    private ArrayAdapter<String> adapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         CalendarView cv = findViewById(R.id.calendarView);
         cv.setDate(System.currentTimeMillis(),false,true);
+
+        addPlantBtn = findViewById(R.id.mainAddPlantBtn);
+        listView = findViewById(R.id.mainListView);
+
+        addPlantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
